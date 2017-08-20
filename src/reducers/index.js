07df92bf45edaedd211dfbeae0f311e3ed5dca38
Dan <danpopa1998@gmail.com>
2017-08-20 12:49:29 +0300

    set initial setup of create react app with custom react scripts and redux

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import navReducer from './navReducer'
+import {combineReducers} from 'redux'
+
+export default combineReducers({
+  nav: navReducer
+})
---
+++ src/reducers/navReducer.js
@@ -0,0 +1,6 @@
+function navReducer (state = 0, action) {
+  console.log(state)
+  return state
+}
+
+export default navReducer
