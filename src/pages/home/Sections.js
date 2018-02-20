458164bb5f132096ab36f634ba105b097f3e175a
Dan <danpopa1998@gmail.com>
2018-02-20 22:15:58 +0200

    add the code for sections

--- src/pages/home/Sections.js
+++ src/pages/home/Sections.js
@@ -1,7 +1,48 @@
 import React, { Component } from 'react'
 import Section from './sections/Section'
+import Loading from 'components/Loading'
+import { connect } from 'react-redux'
+import { fetchImage } from 'actions/imagesActions'
+import { substractDate } from 'actions/dateActions'
+import date from 'date-and-time'
 
 class Sections extends Component {
+
+  componentDidMount () {
+    this.loadSingleImage()
+    
+    document.addEventListener('scroll', event => {
+      const d = document.documentElement
+      const offset = d.scrollTop + window.innerHeight
+      const height = d.offsetHeight
+      
+      if ((height - offset < 200) && !this.props.pending) {
+        console.log('start fetching')
+        this.loadSingleImage()
+        this.loadSingleImage()
+        this.loadSingleImage()
+        this.loadSingleImage()
+      }
+    })
+  }
+
+  showImages () {
+    let data = this.props.images.imagesData
+    return data.map((image, id) => {
+      return <Section url={image.url} key={id} id={id} />
+    })
+  }
+
+  loadSingleImage () {
+    const dateObj = date.parse(this.props.date, 'YYYY/MM/DD')
+    const year = date.format(dateObj, 'YYYY')
+    const month = date.format(dateObj, 'MM')
+    const day = date.format(dateObj, 'DD')
+    console.log(year + ' ' + month + ' ' + day)
+    this.props.fetchImage(year, month, day)
+    this.props.substractDate()
+  }
+
   render () {
     return (
       <div>
@@ -11,4 +52,26 @@ class Sections extends Component {
   }
 }
 
-export default Sections
+const mapStateToProps = state => {
+  return {
+    images: state.images,
+    date: state.date,
+    pending: state.images.pending
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchImage: (year, month, day) => {
+      dispatch(fetchImage(year, month, day))
+    },
+    substractDate: () => {
+      dispatch(substractDate())
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Sections)
