23327b8159aa7575fdfe4dd6fd1ef42ae99c97ef
Dan <danpopa1998@gmail.com>
2018-03-25 20:04:09 +0300

    start adding animations

--- src/pages/home/sections/section/InfoBtn.js
+++ src/pages/home/sections/section/InfoBtn.js
@@ -2,6 +2,7 @@ import React from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
 import styles from './InfoBtn.module.styl'
 import { Link } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
 
 const InfoBtn = props =>
   <div className={styles.info}>
