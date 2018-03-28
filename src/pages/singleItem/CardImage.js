6112b868222800919c1d42953fa16772dcbd3737
Dan <danpopa1998@gmail.com>
2018-03-28 22:03:12 +0300

    add fetch when image not present in redux store

--- src/pages/singleItem/CardImage.js
+++ src/pages/singleItem/CardImage.js
@@ -8,6 +8,8 @@ import {
 } from 'material-ui/Card'
 import { connect } from 'react-redux'
 import { filter, compose, flatten } from 'ramda'
+import { fetchImage } from 'actions/imagesActions'
+import date from 'date-and-time'
 
 function getItemByDate(state, date) {
   const isTheSameDate = (date1, date2) => date1 === date2
@@ -19,24 +21,42 @@ function getItemByDate(state, date) {
 
 @connect((state, ownProps) => ({
   image: getItemByDate(state, ownProps.date)
-}))
+}),
+{
+  fetchImage
+}
+)
 class CardImage extends React.Component {
+  componentDidMount() {
+    if (!this.props.image[0]) {
+      const dateObj = date.parse(this.props.date, 'YYYY/MM/DD')
+      const year = date.format(dateObj, 'YYYY')
+      const month = date.format(dateObj, 'MM')
+      const day = date.format(dateObj, 'DD')
+      this.props.fetchImage(year, month, day)
+    }
+  }
+
   render () {
     const image = this.props.image[0]
-    return (
-      <Card style={{margin: '50px 0'}}>
-        {console.log(this.props)}
-        <CardMedia
-          overlay={<CardTitle title={ `By ${image.copyright}` } />}
-        >
-          <img src={ image.hdurl } alt={ image.title } style={{width: '100%', height: 'auto'}}/>
-        </CardMedia>
-        <CardTitle title={ image.title } subtitle={ `Date: ${ image.date }`}/>
-        <CardText>
-          { image.explanation }
-        </CardText>
-      </Card>
-    )
+    if(image){
+      return (
+        <Card style={{margin: '50px 0'}}>
+          {console.log(this.props)}
+          <CardMedia
+            overlay={<CardTitle title={ `By ${image.copyright}` } />}
+          >
+            <img src={ image.hdurl } alt={ image.title } style={{width: '100%', height: 'auto'}}/>
+          </CardMedia>
+          <CardTitle title={ image.title } subtitle={ `Date: ${ image.date }`}/>
+          <CardText>
+            { image.explanation }
+          </CardText>
+        </Card>
+      )
+    } else {
+      return null
+    }
   }
 }
 
